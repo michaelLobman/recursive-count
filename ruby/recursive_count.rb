14a870b6cf224012cfa880e728db3c49c5a26402
Michael Lobman <michael.lobman@gmail.com>
2022-01-31 16:01:30 -0500

    Complete assignment

--- ruby/recursive_count.rb
+++ ruby/recursive_count.rb
@@ -1,11 +1,10 @@
 def recursive_count(num = 0)
-  # type your code in here
+  unless num >=10 
+    puts num
+    recursive_count(num+1)
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   recursive_count
 end
-
-# OPTIONAL
-# Please add your pseudocode to this file
-# And a written explanation of your solution
